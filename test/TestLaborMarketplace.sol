aa8b5fc332f962bed5a07b8cc5e0002f3f87ae66
Mike Maliszewski <mike@oceansidelabs.io>
2022-01-05 07:12:32 -0600

    added test for labor marketplace

---
+++ test/TestLaborMarketplace.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/LaborMarketplace.sol";
+
+contract TestLaborMarketplace {
+    function testInitialState() public {
+        LaborMarketplace sut = new LaborMarketplace();
+        Assert.equal(sut.getActiveJobCount(), 0, "Should be no initial active jobs");
+    }
+}
